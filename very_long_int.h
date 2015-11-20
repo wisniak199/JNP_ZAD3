c91d47d6d147441ee309b202d665b029f6cd87dc
magdasuchodolska <magda.suchodolska@gmail.com>
2015-11-20 19:28:54 +0100

    szkielet klasy i nagłówki niekórych  operatorów

--- very_long_int.h
+++ very_long_int.h
@@ -1,1 +1,25 @@
 //plik naglowkowy
+
+#include <vector>
+
+typedef vector<int> digit_list;
+
+#ifndef VERYLONGINT_H_
+#define VERYLONGINT_H_
+
+class VeryLongInt {
+private:
+	digit_list digits;
+public:
+	VeryLongInt();
+	~VeryLongInt();
+	VeryLongInt& operator+=(const VeryLongInt &verylongint);
+	VeryLongInt& operator-=(const VeryLongInt &verylongint);
+	VeryLongInt& operator*=(const VeryLongInt &verylongint);
+	VeryLongInt& operator/=(const VeryLongInt &verylongint);
+	VeryLongInt& operator%=(const VeryLongInt &verylongint);
+	VeryLongInt& operator<<=(const VeryLongInt &verylongint);
+	VeryLongInt& operator>>=(const VeryLongInt &verylongint);
+};
+
+#endif /* VERYLONGINT_H_ */
