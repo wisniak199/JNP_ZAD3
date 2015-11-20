d81a4df630f00f408a8eb3662217a711cda35ccb
magdasuchodolska <magda.suchodolska@gmail.com>
2015-11-20 23:44:16 +0100

    dodałam próbę metody /=

--- very_long_int.cc
+++ very_long_int.cc
@@ -1,2 +1,27 @@
 /** Magda: porownania, (-, /, >>, %), operatory
  *  Piotrek: kontruktory, (+, *, <<), BinaryDigits
+ */
+
+// To CHYBA działa
+VeryLongInt& operator/=(const VeryLongInt &denumerator);
+    VeryLongInt temp = 1;
+    VeryLongInt one = 1;
+    VeryLongInt quotient = 0;
+
+    while (this <= denumerator) {
+        denumerator <<= 1;
+        temp <<= 1;
+    }
+
+    while (temp > one) {
+        denumerator >>= 1;
+        temp >>= 1;
+
+        if (this >= denumerator) {
+            this -= denumerator;
+            quotient += temp;
+        }
+    }
+
+    return quotient;
+}
