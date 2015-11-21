e7d3ec351f0cf9bfdc400e47a712a1e57cc9e5f8
magdasuchodolska <magda.suchodolska@gmail.com>
2015-11-21 14:44:12 +0100

    moje bazgroły

--- very_long_int.cc
+++ very_long_int.cc
@@ -1,27 +1,63 @@
+#include "very_long_int.h"
+
 /** Magda: porownania, (-, /, >>, %), operatory
  *  Piotrek: kontruktory, (+, *, <<), BinaryDigits
  */
 
 // To CHYBA działa
-VeryLongInt& operator/=(const VeryLongInt &denumerator);
-    VeryLongInt temp = 1;
-    VeryLongInt one = 1;
-    VeryLongInt quotient = 0;
+VeryLongInt& VeryLongInt::operator/=(const VeryLongInt &denumerator){
+ /*   VeryLongInt temp; = 1;
+    VeryLongInt one; = 1;
+    VeryLongInt quotient; = 0;
 
-    while (this <= denumerator) {
-        denumerator <<= 1;
-        temp <<= 1;
+    while ((*this) <= denumerator) {
+        denumerator <<= one;
+        temp <<= one;
     }
-
     while (temp > one) {
-        denumerator >>= 1;
-        temp >>= 1;
-
-        if (this >= denumerator) {
-            this -= denumerator;
+        denumerator >>= one;
+        temp >>= one;
+        if ((*this) >= denumerator) {
+            (*this) -= denumerator;
             quotient += temp;
         }
     }
+    return quotient; */
+    return *this;
+}
 
-    return quotient;
+const VeryLongInt VeryLongInt::operator-(const VeryLongInt &other) const {
+    return VeryLongInt(*this) -= other;
+}
+
+const VeryLongInt VeryLongInt::operator/(const VeryLongInt &other) const {
+    return VeryLongInt(*this) /= other;
+}
+
+const VeryLongInt VeryLongInt::operator%(const VeryLongInt &other) const {
+    return VeryLongInt(*this) %= other;
+}
+
+const VeryLongInt VeryLongInt::operator>>(const VeryLongInt &other) const {
+    return VeryLongInt(*this) >>= other;
+}
+
+bool VeryLongInt::operator==(const VeryLongInt &other) const{
+
+	// return !(this < other) && !(other < this);
+
+/*	if ((*this).getlen() != other.getlen())
+		return false;
+	else{
+		unsigned long len = (*this).getlen();
+		for (unsigned long i = 0; i < len; i++)
+			if ((*this).getdigits()[i] != other.getdigits()[i])
+				return false;
+		return true;
+	} */
+	return true;
+}
+
+bool VeryLongInt::operator!=(const VeryLongInt &other) const{
+	return !(*this == other);
 }
--- very_long_int.h
+++ very_long_int.h
@@ -10,11 +10,14 @@ typedef std::vector<long int> digit_list;
 class VeryLongInt {
 private:
 	digit_list digits;
+	unsigned long len; //dlugosc vectora - unsigned long?
 	bool NaN;
 	bool Zero;
 public:
 	VeryLongInt();
 	~VeryLongInt();
+	unsigned long getlen(); //getter - pomocnicza
+	digit_list getdigits(); //getter - pomocnicza
 	VeryLongInt& operator+=(const VeryLongInt &verylongint);
 	VeryLongInt& operator-=(const VeryLongInt &verylongint);
 	VeryLongInt& operator*=(const VeryLongInt &verylongint);
