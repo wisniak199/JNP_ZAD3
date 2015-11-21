8294be7f3ae5a530a587243e4d91e2b42e9b005d
Piotr Wiśniewski <pw361286@students.mimuw.edu.pl>
2015-11-21 17:51:45 +0100

    Konstruktory

--- very_long_int.cc
+++ very_long_int.cc
@@ -1,8 +1,56 @@
+#include <string>
+#include <sstream>
 #include "very_long_int.h"
 
 /** Magda: porownania, (-, /, >>, %), operatory
  *  Piotrek: kontruktory, (+, *, <<), BinaryDigits
  */
+
+//zmieniac 2 naraz
+const int base = 10;
+const int base_length = 1;
+
+
+VeryLongInt::VeryLongInt() {
+    digits = digit_list({0L});
+    Zero = true;
+    NaN = false;
+}
+
+VeryLongInt::VeryLongInt(const VeryLongInt &other) : digits(other.digits), Zero(other.Zero), NaN(other.NaN) {}
+
+VeryLongInt::VeryLongInt(VeryLongInt &&other) : digits(std::move(other.digits)), Zero(other.Zero), NaN(other.NaN){}
+
+VeryLongInt::VeryLongInt(unsigned int n) {
+    NaN = false;
+    if (n == 0) {
+        Zero = true;
+        digits.push_back(0);
+    } else {
+        Zero = false;
+        while (n != 0) {
+            digits.push_back(n % base);
+            n /= base;
+        }
+    }
+}
+
+VeryLongInt::VeryLongInt(const std::string &s) {
+    NaN = false;
+    if (s[0] == '0') {
+        Zero = true;
+        digits.push_back(0);
+    } else {
+        std::stringstream stream;
+        for (int i = s.length() - base_length; i > -base_length; i -= base_length) {
+            stream.str(s.substr(std::max(0, i), i + base_length - 1));
+            int temp;
+            stream >> temp;
+            digits.push_back(temp);
+        }
+    }
+}
+
 
 VeryLongInt& VeryLongInt::operator/=(const VeryLongInt &denumerator){
  /*   VeryLongInt temp = 1;
